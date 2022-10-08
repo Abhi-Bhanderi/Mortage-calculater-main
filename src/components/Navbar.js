34c0730a787b0e610b834d74fc31009bdd91306c
Abhi Bhanderi <94031926+Developer4ever@users.noreply.github.com>
2022-10-08 11:33:14 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { BiPlusCircle } from "react-icons/bi";
 import { db } from "../firebase";
-import { updateDoc, doc } from "firebase/firestore";
+import { updateDoc, doc, onSnapshot, getDoc } from "firebase/firestore";
 import { Switch } from "antd";
 
 const Navbar = (props) => {
   const [toggle, setToggle] = useState(false);
   const ref = doc(db, "link", "on_off");
-  const toggler = () =>{
-    toggle ? setToggle(false): setToggle(true)
-  updateToggle()
-  }
 
+  const toggler = () => {
+    toggle ? setToggle(false) : setToggle(true);
+  };
 
-  const updateToggle =async ()=>{
-    if (toggle === true) {
-       return await updateDoc(ref, {
+  useEffect(() => {
+    return async () => {
+      if (toggle === true) {
+        return await updateDoc(ref, {
           is_enabled: false,
         });
+      }
 
-    }
-
-    if (toggle === false) {
-       return await updateDoc(ref, {
+      if (toggle === false) {
+        return await updateDoc(ref, {
           is_enabled: true,
         });
+      }
+    };
+  }, [toggler]);
 
-    }
-  }
+  // const isChecked = () => {
+  //   getDoc(ref).then((doc) => {
+  //     return doc.data().is_enabled;
+  //   });
+  // };
 
   return (
     <header>
@@ -46,15 +51,7 @@ const Navbar = (props) => {
             </li>
           </div>
           <div className="nav-btns">
-            {/* <input
-              type="checkbox"
-              value={checked}
-              id="onOff"
-              name="onOff"
-              onChange={handleCheckbox}
-            /> */}
-
-            <Switch className="toggle-switch" onClick={toggler}/>
+            <Switch className="toggle-switch" onClick={toggler} />
 
             <Link to={props.pageLink} className="nav-btn-link">
               <BiPlusCircle />
